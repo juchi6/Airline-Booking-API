c40cf0e4ff45fedd5853aa93f9da30933912556d
juchi <jenninduka90@gmail.com>
2026-08-14 20:56:49 +0100

    Add AirportRepository

--- src/repositories/index.js
+++ src/repositories/index.js
@@ -1,5 +1,6 @@
 module.exports = {
     AirplaneRepository: require("./airplane-repository"),
     UserRepository: require("./user-repository"),
-    CityRepository: require("./city-repository")
+    CityRepository: require("./city-repository"),
+    AirportRepository: require("./airport-repository")
 }
