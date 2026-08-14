453aaffb77b921cfe84eeccef09c31d43d29c237
juchi <jenninduka90@gmail.com>
2026-08-14 20:07:54 +0100

    Require admin authentication to create airplanes

--- src/routes/v1/airplane-routes.js
+++ src/routes/v1/airplane-routes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { AirplaneController } = require("../../controllers");
-const { validate } = require("../../middlewares");
+const { validate, protect, authorize } = require("../../middlewares");
 const { createAirplaneSchema } = require("../../validators");
 
-router.post("/", validate(createAirplaneSchema), AirplaneController.createAirplane);
+router.post("/", protect, authorize("admin"), validate(createAirplaneSchema), AirplaneController.createAirplane);
 
 module.exports = router;
