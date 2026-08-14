b679f775c721d5b7be3d0705fd976b72ebbce3f9
juchi <jenninduka90@gmail.com>
2026-08-14 19:46:54 +0100

    Remove leftover debug middleware from airplane routes

--- src/routes/v1/airplane-routes.js
+++ src/routes/v1/airplane-routes.js
@@ -1,11 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const { AirplaneRepository } = require("../../repositories");
-const { AirplaneController } = require("../../controllers");    
+const { AirplaneController } = require("../../controllers");
 
-router.post("/", (req, res, next) => {
-    console.log("Currently inside routes/v1/airplane-routes.js");
-    next();
-}, AirplaneController.createAirplane);
+router.post("/", AirplaneController.createAirplane);
 
 module.exports = router;
