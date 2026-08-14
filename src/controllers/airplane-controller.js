6dcafce58c0621e3821f03e91f683cf7406c67b4
juchi <jenninduka90@gmail.com>
2026-08-14 19:46:09 +0100

    Adopt catchAsync in airplane controller, remove manual try/catch

--- src/controllers/airplane-controller.js
+++ src/controllers/airplane-controller.js
@@ -1,23 +1,19 @@
 const { StatusCodes } = require("http-status-codes");
 const { AirplaneService } = require("../services");
+const { catchAsync } = require("../utils");
 
-async function createAirplane(req, res, next) {
-    console.log("Currently inside controllers/airplane-controller.js");
-    try {
-        const airplane = await AirplaneService.createAirplane({
-            modelNumber: req.body.modelNumber,
-            capacity: req.body.capacity
-        });
-        return res.status(StatusCodes.CREATED).json({
-            data: airplane,
-            success: true,
-            message: "Successfully created a airplane",
-            err: {}
-        });
-    } catch (error) {
-        return next(error);
-    }
-}
+const createAirplane = catchAsync(async (req, res) => {
+    const airplane = await AirplaneService.createAirplane({
+        modelNumber: req.body.modelNumber,
+        capacity: req.body.capacity
+    });
+    return res.status(StatusCodes.CREATED).json({
+        data: airplane,
+        success: true,
+        message: "Successfully created a airplane",
+        err: {}
+    });
+});
 
 module.exports = {
     createAirplane
