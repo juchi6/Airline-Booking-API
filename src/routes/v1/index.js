e986f39551514f635db32f203529729a7276ebfe
juchi <jenninduka90@gmail.com>
2026-08-14 20:06:09 +0100

    Wire auth routes for register and login

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -3,10 +3,12 @@ const express = require("express");
 const { InfoController } = require("../../controllers");
 
 const airplaneRoutes = require("./airplane-routes");
+const authRoutes = require("./auth-routes");
 
 const router = express.Router();
 
 router.use("/airplanes", airplaneRoutes);
+router.use("/auth", authRoutes);
 
 router.get("/info", InfoController.info);
 
